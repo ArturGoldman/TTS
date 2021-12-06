1ce138bc8567a6c619cae415d480f51276c08a06
ArturGoldman <arturgoldman184@gmail.com>
2021-12-06 22:20:20 +0300

    Test fix

--- hw_tts/base/base_trainer.py
+++ hw_tts/base/base_trainer.py
@@ -186,7 +186,7 @@ class BaseTrainer:
             )
         else:
             self.optimizer.load_state_dict(checkpoint["optimizer"])
-            self.lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+            self.lr_scheduler.load_state_dict(checkpoint["scheduler"])
 
         self.logger.info(
             "Checkpoint loaded. Resume training from epoch {}".format(self.start_epoch)
